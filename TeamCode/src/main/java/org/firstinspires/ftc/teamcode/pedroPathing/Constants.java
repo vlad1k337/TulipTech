a0a81f693d5a2cecebbed1949157ed81472e626e
vlad1k337 <vladguy.baranovskii@gmail.com>
2025-10-24 13:07:47 -0500

    Robot rehaul

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,17 +3,47 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(4.5);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(0.65)
+            .rightFrontMotorName("FR")
+            .rightRearMotorName("BR")
+            .leftRearMotorName("BL")
+            .leftFrontMotorName("FL")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+    public static PinpointConstants localizerPinpointConstants = new PinpointConstants()
+            .forwardPodY(-4)
+            .strafePodX(3.5)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .pinpointLocalizer(localizerPinpointConstants)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TulipTeleOp.java
@@ -0,0 +1,136 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+@Configurable
+@TeleOp
+public class TulipTeleOp extends OpMode {
+    private Supplier<PathChain> pathChain;
+    private DcMotor launchR, launchL;
+
+    private Follower follower;
+    private TelemetryManager telemetryM;
+
+    public static Pose startingPose;
+
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+
+    private double launchSpeedRight = 0.0;
+    private double launchSpeedLeft  = 0.0;
+
+    public void initAprilTag()
+    {
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(2);
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
+                .build();
+    }
+
+    @Override
+    public void init()
+    {
+        follower = Constants.createFollower(hardwareMap);
+
+        startingPose = new Pose(0, 0);
+        follower.setStartingPose(startingPose);
+        follower.update();
+        initAprilTag();
+
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        launchR = hardwareMap.get(DcMotor.class, "launch R");
+        launchL = hardwareMap.get(DcMotor.class, "launch L");
+
+        pathChain = () -> follower.pathBuilder()
+                .addPath(new Path(new BezierLine(follower::getPose, new Pose(72, 72))))
+                .setConstantHeadingInterpolation(45.0)
+                .build();
+    }
+
+    @Override
+    public void start()
+    {
+        follower.startTeleopDrive();
+        follower.update();
+    }
+
+    public void update_shooters()
+    {
+        if(gamepad1.xWasPressed())
+        {
+            launchSpeedLeft  = 0.5;
+            launchSpeedRight = 0.5;
+        } else if(gamepad1.yWasPressed()) {
+            launchSpeedLeft  = 0.0;
+            launchSpeedRight = 0.0;
+        }
+
+        if(gamepad1.leftBumperWasPressed()) {
+            if (launchSpeedLeft - 0.1 > 0.0) launchSpeedLeft -= 0.1;
+            if (launchSpeedRight - 0.1 > 0.0) launchSpeedRight -= 0.1;
+        } else if(gamepad1.rightBumperWasPressed()) {
+            if(launchSpeedLeft  + 0.1 < 1.0) launchSpeedLeft  += 0.1;
+            if(launchSpeedRight + 0.1 < 1.0) launchSpeedRight += 0.1;
+        }
+
+        telemetryM.debug("Left Shooter power: " + launchSpeedLeft);
+        telemetryM.debug("Right Shooter power: " + launchSpeedRight);
+
+        launchR.setPower(-launchSpeedRight);
+        launchL.setPower(launchSpeedLeft);
+    }
+
+    @Override
+    public void loop()
+    {
+        follower.update();
+
+        follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x,
+                true
+        );
+
+        update_shooters();
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for(AprilTagDetection detection : currentDetections)
+        {
+            if(detection.metadata != null)
+            {
+                telemetryM.debug("Tag Name: " + detection.metadata.name);
+                telemetryM.debug("Tag Pose (x, y, z): " + detection.ftcPose.x + ", " + detection.ftcPose.y + ", " + detection.ftcPose.z);
+            }
+        }
+
+        telemetryM.debug("x:" + follower.getPose().getX());
+        telemetryM.debug("y:" + follower.getPose().getY());
+        telemetryM.debug("heading:" + follower.getPose().getHeading());
+
+        telemetryM.update(telemetry);
+
+    }
+}
